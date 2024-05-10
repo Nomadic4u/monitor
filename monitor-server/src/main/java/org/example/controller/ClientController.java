5b6e78ffed5ea4049b1617674931382db1e5a4d0
Nomadic4u <2910887794@qq.com>
2024-05-10 10:47:55 +0800

    5.10

--- monitor-server/src/main/java/org/example/controller/ClientController.java
+++ monitor-server/src/main/java/org/example/controller/ClientController.java
@@ -20,12 +20,24 @@ public class ClientController {
     @Resource
     ClientService clientService;
 
+    /**
+     * 用户客户端注册
+     * @param token 服务器上的token
+     * @return 注册是否成功, token是否有效
+     */
+
     @GetMapping("/register")
     public RestBean<Void> registerClient(@RequestHeader("Authorization") String token) {
         return clientService.verifyAndRegister(token) ?
                 RestBean.success() : RestBean.failure(401, "token无效, 注册失败");
     }
 
+    /**
+     * 用户客户端给服务端发送详细信息
+     * @param client 客户端实体
+     * @param vo 客户端详细信息
+     * @return 是否发送成功
+     */
     @PostMapping("/detail")
     public RestBean<Void> updateClientDetails(@RequestAttribute(Const.ATTR_CLIENT) Client client,
                                               @RequestBody @Valid ClientDetailVO vo) {
@@ -33,6 +45,12 @@ public class ClientController {
         return RestBean.success();
     }
 
+    /**
+     * 用于客户端给服务端发送运行时信息
+     * @param client 客户端实体
+     * @param vo 运行时信息
+     * @return 是否发送成功
+     */
     @PostMapping("/runtime")
     public RestBean<Void> updateRuntimeDetails(@RequestAttribute(Const.ATTR_CLIENT) Client client,
                                                @RequestBody @Valid RuntimeDetailVO vo) {
